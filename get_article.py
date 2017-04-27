a02cd5101e3532ecb28f296579ccca4a4656760f
Ling1992 <l_ing1992@163.com>
2017-04-27 17:11:29 +0800

    spider

--- get_article.py
+++ get_article.py
@@ -5,6 +5,7 @@ import MySQLdb
 
 class MysqlLing(object):
     db = None
+    cursor = None
     __instance = None
 
     def __init__(self):
@@ -14,28 +15,43 @@ class MysqlLing(object):
         if not cls.__instance:
             cls.__instance = super(MysqlLing, cls).__new__(cls, *args, **kwargs)
             cls.db = MySQLdb.connect("localhost", "root", "root", "ling_python_test1", charset="utf8")
+            cls.cursor = cls.db.cursor()
+            print 'MysqlLing __new__ '
         return cls.__instance
 
+    def insert(self, sql_str):
+        print sql_str
+        try:
+            self.cursor.execute(sql_str)
+            # insert_id = self.db.insert_id()
+            # insert_id = self.cursor.lastrowid
+            self.db.commit()
+            return True
+        except Exception, e:
+            print e
+            self.db.rollback()
+            print 'rollback'
+            return  False
+
+    def count(self, sql_str):
+        count = self.cursor.execute(sql_str)
+        return count
+
     def __del__(self):
+        self.cursor.close()
         self.db.close()
         print 'db closed !'
 
 
 if __name__ == '__main__':
-
-    # ling = MysqlLing()
-    # cursor = ling.db.cursor()
-    #
     # sql = "insert into article(title, article) values ('%s', '%s') " % ('test_title ', '啊哈哈哈哈')
-    #
-    # try:
-    #     cursor.execute(sql)
-    #     ling.db.commit()
-    # except:
-    #     ling.db.rollback()
-
-    aa = 'asdlfjasl%sasdfla%ss'
-    bb = aa % ('埃里克森的', '说了打飞机阿里圣诞节')
-    print aa
-    print bb
+    # sql = "select * from article where title='aaaa'"
+    # sql = "insert into article(article_id, title, article) values ('%d', '%s', '%s') " % (11, 'test_title ', '啊哈哈哈哈')
+    sql = "insert into article_list(title, abstract, article_id, group_id, category_id) values ('%s', '%s', '%d', '%d', '%d') " % ('asdfjl', 'test_title ', 12, 131, 11)
+    mysql = MysqlLing()
+    res = mysql.insert(sql)
+    if res:
+        print 'yes', res
+    else:
+        print 'no'
 
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # _*_ encoding=utf-8 _*_
 
 import requests
-import json
 from bs4 import BeautifulSoup
+from get_article import MysqlLing
+import time
+import json
 
 domain = 'http://www.toutiao.com'
 
@@ -32,56 +34,90 @@ print ''.join([domain, title_url])
 
 title_respond = requests.get(''.join([domain, title_url]))
 
-first_list_url = 'http://www.toutiao.com/api/pc/feed/?category=news_hot&utm_source=toutiao&widen=1&max_behot_time=0&max_behot_time_tmp=0&tadrequire=false'
+title_list_url = 'http://www.toutiao.com/api/pc/feed/?category=news_hot&utm_source=toutiao&widen=1&max_behot_time=0&max_behot_time_tmp=0&tadrequire=true&as=A11539D0713B436&cp=59013B14E316DE1'
 
+url = title_list_url
 
-title_list_url = 'http://www.toutiao.com/api/pc/feed/?category=news_hot&utm_source=toutiao&widen=1&max_behot_time=%s&max_behot_time_tmp=%s&tadrequire=false'
+while 1:
+    print url
+    title_list_respond = requests.get(url)
+    print title_list_respond.status_code, title_list_respond.reason
+    if title_list_respond.status_code != 200:
+        break
+    title_list_content = json.loads(title_list_respond.content)
+    behot_time = title_list_content['next']['max_behot_time']
+    url = title_list_url
+    print 'max_behot_time' , title_list_content['next']     # max_behot_time
 
-url = title_list_url % ('1493193588', '1493193588')
-print url
+    for item in title_list_content['data']:
+        print 'has_gallery', item.get('has_gallery')
+        print 'source_url:', item.get('source_url')
+        if item.get('has_gallery'):
+            continue
+        if item.get('source_url') == '':
+            continue
+        article_url = ''.join([domain, item.get('source_url')])
+        print article_url
+        article_respond = requests.get(article_url)
+        print article_respond.status_code
+        if article_respond.status_code != 200:
+            continue
+        """
+            bs4 html 操作
+        """
+        soup = BeautifulSoup(article_respond.content, 'html.parser')
+        header = soup.select('header > h1')
+        if not header or not len(header):
+            print 'header if 1'
+            header = soup.find('h1', class_='article-title')
+        else:
+            header = header[0]
+        if not header or not len(header):
+            raise Exception('header is none or []')
 
-title_list_respond = requests.get(url)
+        print header
+        article = soup.find('article')
+        if not article or not len(article):
+            print 'article if 1'
+            article = soup.find_all('figure')
+        if not article or not len(article):
+            print 'article if 2'
+            article = soup.find('div', class_='article-content')
+        if not article or not len(article):
+            raise Exception('article is none or []')
+        print article
 
-with open('title_list_content.txt', 'w') as f:
-    f.write(title_list_respond.content)
+        """
+            sql 操作
+        """
+        ling_con = MysqlLing()
+        count = ling_con.count("select * from article_list where group_id='%s'"
+                               % item.get('group_id'))
+        print count
+        if count:
+            print '数据 重复！！！'
+            res = False
+        else:
+            print '新增 数据 ！！！'
+            res = ling_con.insert("insert into article_list(title, abstract, tag, group_id, original_time) VALUES ('%s', '%s', '%s', '%s', '%s')"
+                            % (item.get('title'), item.get('abstract'), item.get('tag'), item.get('group_id'), item.get('behot_time')))
+            print res
+            if res:
+                pass
+            else:
+                raise Exception('insert article_list error ')
+            res = ling_con.insert("INSERT INTO article(article_id, title, article) VALUES ('%s', '%s', '%s')"
+                                  % (item.get('group_id'), header, article))
+            print res
+            if res:
+                print 'insert article success !!'
+            else:
+                raise Exception('insert article error ')
 
-first_list_respond = requests.get(first_list_url)
-print first_list_respond.status_code, first_list_respond.reason
-first_list_content = json.loads(first_list_respond.content)
-# print first_list_content['data']    # data
-max_behot_time = first_list_content['next']['max_behot_time']
-print 'max_behot_time' , first_list_content['next']     # max_behot_time
+        print "\n"
+        time.sleep(1)
+    time.sleep(2)
 
-with open('first_list_content.txt', 'w') as f:
-    f.write(first_list_respond.content)
-index1 = 1
-for item in first_list_content['data']:
-    print item.get('has_gallery')
-    print item.get('source_url')
-    if item.get('has_gallery') == 'True':
-        continue
-    if item.get('source_url') == '':
-        continue
-    article_url = ''.join([domain, item.get('source_url')])
-    print article_url
-    article_respond = requests.get(article_url)
-    with open('article_content%s.html' % (index1), 'w') as f:
-        f.write(article_respond.content)
-    soup = BeautifulSoup(article_respond.content, 'html.parser')
-    header = soup.select('header > h1')
-    print 'bb'
-    if not header or len(header):
-        print 'aa'
-        header = soup.find('h1', class_='article-title')
-
-    print header
-    article = soup.find('article')
-    if not article or len(article):
-        article = soup.find_all('figure')
-    print article
-    print "\n"
-
-print title_list_url % (max_behot_time, max_behot_time)
 
 
 
